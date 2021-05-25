3ecb3c3c8b2a0a95221cbd62a932c4e153a4e2dd
Zorgulle <winy95battle@gmail.com>
2021-05-25 21:11:39 +0000

    Add price currency and stock price

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import requests
+from lxml import etree
+from io import StringIO
+
+def parse_content(content):
+    infos_to_xpath = {
+        "price": '//*[@id="main-content"]/div/section[1]/header/div/div/div[1]/div[1]/div/div[1]/span[1]',
+        "currency": '//*[@id="main-content"]/div/section[1]/header/div/div/div[1]/div[1]/div/div[1]/span[3]',
+    }
+
+    parser = etree.HTMLParser()
+    tree = etree.parse(StringIO(content), parser)
+    return {i: tree.xpath(x)[0].text.strip() for i, x in infos_to_xpath.items()}
 
 def get_stock_infos(stock_code):
     url = "https://boursorama.com/cours/%s" % (stock_code)
-    print("Calling: %s" % url)
     response = requests.get(url)
     response.raise_for_status()
-    return response.content
+    infos = parse_content(response.text)
+    return infos
     
 
 if __name__ == '__main__':
@@ -17,4 +29,5 @@ if __name__ == '__main__':
             print(row[0], row[1])
             if index == 0:
                 continue
-            get_stock_infos(row[0]) 
+            infos = get_stock_infos(row[0]) 
+            print(infos)
